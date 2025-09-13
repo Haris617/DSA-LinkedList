fe90afe846d537ac3f400ab38cb21c56881ea608
Haris Wasim <toughrider617@gmail.com>
2025-09-13 20:36:58 +0500

    Display.cpp
    Displaying Link List
    1. From Head to Tail
    2. Based on Value

--- LinkedList/display.cpp
+++ LinkedList/display.cpp
@@ -1,1 +1,29 @@
+void display() // Displaying Link List From Head  to Tail
+{
+	node* temp = head;
+
+	while (temp != nullptr)
+	{
+		cout << "Data is : " << temp->data << endl;
+		temp = temp->next;
+	}
+}
+
+void searchValue(int val) // Display Data if it Exists
+{
+	node* temp = head;
+
+	while (temp!= nullptr)
+	{
+		if (val == temp->data)
+		{
+			cout << "Value Found" << endl;
+			return;
+		}
+
+		temp = temp->next;
+	}
+
+	cout << "Value not Found" << endl;
+}
 
