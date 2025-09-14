dbb2dfcba42e1f0f01cb9e2a409e46d8602e519f
Haris Wasim <toughrider617@gmail.com>
2025-09-14 21:20:42 +0500

    Update delete.cpp
    1. Delete Node at Start
    2. Delete Node at End (if Tail is There)
    3. Delete Node at End (if Tail not There)

--- LinkedList/delete.cpp
+++ LinkedList/delete.cpp
@@ -52,3 +52,32 @@ void deleteAtEnd() // Deleting Node at The End
 
 	}
 }
+
+void deleteAtEndnHeadOnly() // Deleting Last Node Without Tail Pointer
+{
+	node* temp = head;
+
+	if (head == nullptr)
+	{
+		cout << "Link List Doesnt Exist" << endl;
+		return;
+	}
+
+	else if (head->next == nullptr)
+	{
+		delete temp;
+		cout << "Node Sucessfully Deleted" << endl;
+		cout << "Link List is Empty Now" << endl;
+		return;
+
+	}
+
+	while (temp->next->next != nullptr)
+	{
+		temp = temp->next;
+	}
+
+	delete temp->next;
+	temp->next = nullptr;
+}
+
