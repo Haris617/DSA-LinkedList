d58b7a2781032573acc093912c875359a59dd6c1
Haris Wasim <toughrider617@gmail.com>
2025-09-14 20:43:51 +0500

    Update delete.cpp
    1. Delete Node at Start

--- LinkedList/delete.cpp
+++ LinkedList/delete.cpp
@@ -1,1 +1,21 @@
+void deleteAtStart()
+{
+	node* temp = head;
 
+	if (head == nullptr)
+	{
+		cout << "link List Dont Exist" << endl;
+		return;
+	}
+
+	else if (head->next == nullptr)
+	{
+		delete head;
+	}
+
+	else
+	{
+		head = head->next;
+		delete temp;
+	}
+}
