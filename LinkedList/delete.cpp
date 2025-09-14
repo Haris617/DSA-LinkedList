65ff41c5cca99a0ce4e42f13f10c57457e1385ba
Haris Wasim <toughrider617@gmail.com>
2025-09-14 21:08:06 +0500

    Update delete.cpp

--- LinkedList/delete.cpp
+++ LinkedList/delete.cpp
@@ -1,4 +1,4 @@
-void deleteAtStart()
+void deleteAtStart() //Deleting Node At Start
 {
 	node* temp = head;
 
@@ -17,5 +17,38 @@ void deleteAtStart()
 	{
 		head = head->next;
 		delete temp;
+	}
+}
+
+void deleteAtEnd() // Deleting Node at The End
+{
+	node* temp = head;
+
+	if (head == nullptr)
+	{
+		cout << "Link List Doesnt Exist" << endl;
+		return;
+	}
+
+	else if (head->next == nullptr)
+	{
+		delete temp;
+		cout << "Node Sucessfully Deleted" << endl;
+		cout << "Link List is Empty Now" << endl;
+		return;
+
+	}
+
+	else
+	{
+		while (temp->next != tail)
+		{
+			temp = temp->next;
+		}
+
+		delete tail;
+		tail = temp;
+		tail->next = nullptr;
+
 	}
 }
