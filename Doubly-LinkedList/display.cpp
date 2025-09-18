0fbe0c88bc2658d22d0c7e3541ebcfef1aaef594
Haris Wasim <toughrider617@gmail.com>
2025-09-18 19:14:48 +0500

    Update display.cpp

--- Doubly-LinkedList/display.cpp
+++ Doubly-LinkedList/display.cpp
@@ -1,39 +1,41 @@
-//Displaying Forward
-
 void displayForward()
 {
     node* temp = head;
 
-    if (head == nullptr) {
+    if (head == nullptr) 
+    {
         cout << "List is empty" << endl;
         return;
     }
 
     cout << "Displaying Forward" << endl;
 
-    while (temp != nullptr) {
+    while (temp != nullptr) 
+    {
         cout << temp->data << " ";
         temp = temp->next;
     }
+
     cout << endl;
 }
-
-//Displaying Backward
 
 void displayBackward()
 {
     node* temp = tail;
 
-    if (tail == nullptr) {
+    if (tail == nullptr)
+    {
         cout << "List is empty" << endl;
         return;
     }
 
     cout << "Displaying Backward" << endl;
 
-    while (temp != nullptr) {
+    while (temp != nullptr) 
+    {
         cout << temp->data << " ";
         temp = temp->previous;
     }
+
     cout << endl;
 }
