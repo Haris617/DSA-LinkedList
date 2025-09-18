8a0f8bdee112adb24878bc3e7e207f5d64a85aac
Haris Wasim <toughrider617@gmail.com>
2025-09-18 19:13:16 +0500

    Update insert.cpp

--- Doubly-LinkedList/insert.cpp
+++ Doubly-LinkedList/insert.cpp
@@ -1,46 +1,59 @@
-#include <iostream>
-using namespace std;
 
-void insertAtStart(int val) {
+void insertAtStart(int val)
+{
     node* n = new node;
     n->data = val;
     n->previous = nullptr;
     n->next = nullptr;
 
-    if (head == nullptr) {
+    if (head == nullptr)
+    {
         head = tail = n;
-    } else {
+    }
+
+    else 
+    {
         n->next = head;
         head->previous = n;
         head = n;
     }
 }
 
-void insertAtEnd(int val) {
+void insertAtEnd(int val)
+{
     node* n = new node;
     n->data = val;
     n->previous = nullptr;
     n->next = nullptr;
 
-    if (head == nullptr) {
+    if (head == nullptr)
+    {
         head = tail = n;
-    } else {
+    }
+
+    else
+    {
         tail->next = n;
         n->previous = tail;
         tail = n;
     }
 }
 
-void insertAtSpecificPosition(int val, int pos) {
+void insertAtSpecificPosition(int val, int pos)
+{
     node* n = new node;
     n->data = val;
     n->previous = nullptr;
     n->next = nullptr;
 
-    if (head == nullptr) {
+    if (head == nullptr) 
+    {
         head = tail = n;
         return;
-    } else if (pos == 1) {
+    }
+
+    else if (pos == 1)
+    {
         n->next = head;
         head->previous = n;
         head = n;
@@ -48,11 +61,13 @@ void insertAtSpecificPosition(int val, int pos) {
     }
 
     node* Previous = head;
-    for (int i = 1; i < pos - 1 && Previous->next != nullptr; i++) {
+    for (int i = 1; i < pos - 1 && Previous->next != nullptr; i++) 
+    {
         Previous = Previous->next;
     }
 
-    if (Previous == tail) {
+    if (Previous == tail) 
+    {
         tail->next = n;
         n->previous = tail;
         tail = n;
